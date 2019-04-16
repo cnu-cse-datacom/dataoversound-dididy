e681cacc451d736f5b9dfe9263fb3bc4e6e2a2ae
Yongjae Yi <dididy@Yongjaes-MacBook-Pro.local>
2019-04-17 06:50:40 +0900

    Finally I did it... ㅠ

--- app/src/main/java/com/example/sound/devicesound/Listentone.java
+++ app/src/main/java/com/example/sound/devicesound/Listentone.java
@@ -27,7 +27,7 @@ public class Listentone {
     int FEC_BYTES = 4;
 
     private int mAudioSource = MediaRecorder.AudioSource.MIC;
-    private int mSampleRate = 44100;
+    private int mSampleRate = 44100; // decode.py의 frame_rate에 해당
     private int mChannelCount = AudioFormat.CHANNEL_IN_MONO;
     private int mAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
     private float interval = 0.1f;
@@ -36,17 +36,17 @@ public class Listentone {
 
     public AudioRecord mAudioRecord = null;
     int audioEncodig;
-    boolean startFlag;
+    boolean startFlag; // decode.py의 in_packet에 해당
     FastFourierTransformer transform;
 
     // def dominanat(frame_rate, chunk):
     private double findFrequency(double[] toTransform) {
         int len = toTransform.length;
-        double[] real = new double[len];
-        double[] img = new double[len];
-        double realNum; // 실수부
-        double imgNum; // 허수부
-        double[] mag = new double[len];
+        double[] real = new double[len]; // 실수 배열 선언
+        double[] img = new double[len]; // 허수 배열 선언
+        double realNum; // 실수
+        double imgNum; // 허수
+        double[] mag = new double[len]; // fft 변환된 signal 을 담는 배열
 
 
         Complex[] complx = transform.transform(toTransform, TransformType.FORWARD);
@@ -58,7 +58,7 @@ public class Listentone {
             imgNum = complx[i].getImaginary();
             // #w = np.fft.fft(chunk)
             // 실수부와 허수부로 이루어진 fft 변환된 signal array를 얻을 수 있도록 함
-            mag[i] = Math.sqrt((realNum * realNum) + (imgNum * imgNum));
+            mag[i] = Math.sqrt((realNum * realNum) + (imgNum * imgNum)); // 신호에 대해 fft 함수를 사용하면 푸리에 변환되어 주파수 영역으로 변환
         }
         // #peak_coeff = np.argmax(np.abs(w))
         double peak_coeff = 0;
@@ -78,7 +78,7 @@ public class Listentone {
     }
 
     // numpy.fft.fftfreq()
-    private Double[] fftfreq(int length, int sample_spacing) {
+    private Double[] fftfreq(int length, int sample_spacing) { // 샘플 수와 샘플링 인터벌에 대해 fft 후의 주파수 값을 반환
         double val = 1.0 / (length * sample_spacing); // #val = 1.0 / (n * d)
         // #results = empty(n, int)
         int [] results = new int[length];
@@ -113,7 +113,7 @@ public class Listentone {
 
     }
 
-    public int findPowerSize(int blocksize_in) { // 4096을 반환 - 2205와 가장 근접한 2의 제곱수는 4096이 맞긴함
+    public int findPowerSize(int blocksize_in) { // 4096을 반환 - 2205보다 큰 가장 작은 2의 제곱수는 4096이 맞긴함
         int power = 1;
         do {
             power *= 2;
@@ -131,28 +131,26 @@ public class Listentone {
     //def extract_packet(freqs):
     public List<Integer> extract_packet(Double [] freqs_in) {
         int freq_length = freqs_in.length;
-        double [] freqs = new double[freq_length];
+//        double [] freqs = new double[freq_length];
         int [] bit_chunks = new int[freq_length];
-
-        int half = 1;
-        // #freqs = freqs[::2]
-        for(int i = 0; i < freq_length / 2; i++) {
-            freqs[i] = freqs_in[half];
-            half = half + 2;
-        }
+//나는왜이작업을안해도되는지몹시궁금함
+//        int half = 0;
+//        // #freqs = freqs[::2]
+//        for(int i = 0; i < freq_length / 2; i++) {
+//            freqs[i] = freqs_in[half];
+//            half = half + 1;
+//        }
         // #bit_chunks = [int(round((f - START_HZ) / STEP_HZ)) for f in freqs]
-        for(int i = 0; i < freq_length / 2; i++) {
-            bit_chunks[i] = (int) Math.round((freqs[i] - START_HZ) / STEP_HZ);
+        for(int i = 0; i < freq_length; i++) {
+            bit_chunks[i] = (int) Math.round((freqs_in[i] - START_HZ) / STEP_HZ);
         }
         int [] bit_chunks_calc = new int[freq_length];
-        int index = 0;
-        for(int i = 1; i < freq_length / 2; i++) {
+        for(int i = 1; i < freq_length; i++) {
             if(bit_chunks[i] >= 0 && bit_chunks[i] <= Math.pow(2, BITS)) {
-                bit_chunks_calc[index] = bit_chunks[i];
-                index++;
+                bit_chunks_calc[i - 1] = bit_chunks[i];
             }
         }
-
+//        Log.d("why..", bit_chunks_calc.toString());
         // #return bytearray(decode_bitchunks(BITS, bit_chunks))
         List<Integer> chunks_fin = decode_bitchunks(BITS, bit_chunks_calc);
         return chunks_fin;
@@ -221,7 +219,7 @@ public void PreRequest() {
     int blocksize = findPowerSize((int) (long) Math.round(interval / 2 * mSampleRate)); // #listen_linux의 num_frames에 해당
 
 
-    //Log.d("blocksize", ""+blocksize); // 4096이 뜨는데 2205와 가장 가까운 2의 제곱수는 4096이 맞음
+    //Log.d("blocksize", ""+blocksize); // 4096이 뜨는데 2205보다 큰 가장 작은 2의 제곱수는 4096이 맞음
 
 
     short[] buffer = new short[blocksize]; // buffer에 소리데이터가 들어감
@@ -253,7 +251,7 @@ public void PreRequest() {
 
                 /* decode.py에서 reed solomon으로 encode된 데이터들을 decode
                    에러가 있다면 해당 원소를 출력
-                   이게 없어서 깨지게 출력될 수 있음 // 상관없
+                   이게 없어서 깨지게 출력될 수 있음 // 없어도 상관없다고하는데 사실 잘 모르겠음
                 #try:
                 #    byte_stream = RSCodec(FEC_BYTES).decode(byte_stream)
                 #    byte_stream = byte_stream.decode("utf-8")
@@ -262,29 +260,27 @@ public void PreRequest() {
                 #except ReedSolomonError as e:
                 #    print("{}: {}".format(e, byte_stream))
                  */
-                Log.d("packet : ", packet.toString());
-                Double[] packet_to_array = packet.toArray(new Double[packet.size()]);
-                // #byte_stream = extract_packet(packet)
+                Double[] packet_to_array = packet.toArray(new Double[packet.size()]); // extract_packet의 매개변수를 배열로 했기에 리스트를 배열로 변환
                 byte_stream = extract_packet(packet_to_array);
-                Log.d("List data : ", Arrays.toString(packet_to_array));
-                String sentence = "";
+                Log.d("packet", byte_stream.toString()); // 주파수를 변환한 결과보여줌
+                String out_string = ""; // 출력할 문자열 준비
                 for (int i = 0; i < byte_stream.size(); i++) {
-                    sentence = sentence + Character.toString((char) ((int) byte_stream.get(i)));
-                }
-                Log.d("test", sentence);
+                    out_string += (char)((int)byte_stream.get(i));
+                } // 문자열에 해당 ascii append함
+                Log.d("result", out_string); // 결과물 출력
                 // #packet = []
                 packet.clear();
                 byte_stream.clear();
                 // #in_packet = False
-                startFlag = false;
+                startFlag = false; // 할거 다했으니 startFlag를 flase로 바꿈
                 // #elif in_packet:
-            } else if (startFlag) {
+            } else if (startFlag) { // startFlag가 false 에서 true로 바뀌면
                 // #packet.append(dom)
-                packet.add(dom);
+                packet.add(dom); // 리스트에 주파수를 계속 append!
                 // #elif match(dom, HANDSHAKE_START_HZ):
-            } else if (match(dom, HANDSHAKE_START_HZ)) {
+            } else if (match(dom, HANDSHAKE_START_HZ)) { // HANDSHAKE_START_HZ를 인식
                 // #in_packet = True
-                startFlag = true;
+                startFlag = true; // startFlag를 true로 바꿈
             }
 
         }
